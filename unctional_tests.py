90a75408735491c66eb00eaadd149bbd1420938d
gubaoer <gubaoer@hotmail.com>
2015-11-06 16:28:30 +0800

    Functional test now checks we can input a to-do item

--- unctional_tests.py
+++ unctional_tests.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,9 +14,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # 打开首页
         self.browser.get('http://127.0.0.1:8000')
 
+        # 确定断网页的标题和头部都包含了'TO-DO'这个词
         self.assertIn('To-Do', self.browser.title)
+        head_text = self.browser.find_element_by_id('h1').text
+        self.assertIn('To-Do', head_text)
+
+        # 输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 然后输入一个任务项
+        inputbox.send_keys("1: Buy peacock feathers")
+
+        # 按回车键之后,页面更新了
+        # 并且办事项显示了相应的信息
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows) # any() ?
+        )
+
+        # 此后页面中又显示了一个文本框,可以输入其他的待办事项,接着她又输入了......
+
         self.fail('Finish the test')
 
 if __name__ in '__main__':
