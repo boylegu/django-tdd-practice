31f048e8fca1c4983d96b8be402148e4e2b32f49
gubaoer <gubaoer@hotmail.com>
2015-11-07 22:03:41 +0800

    6.3

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-import time # 为了调试,可用time.sleep
+import time  # 为了调试,可用time.sleep
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -38,10 +38,11 @@ class NewVisitorTest(LiveServerTestCase):
         # 然后输入一个任务项
         inputbox.send_keys("Buy peacock feathers")
 
-        # 按回车键之后,页面更新了
-        # 并且办事项显示了相应的信息
+        # 按回车键之后,被带到了一个新的url
+        # 并且这个新的页面显示了相应的信息
         inputbox.send_keys(Keys.ENTER)
-
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -50,7 +51,35 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.fail('Finish the test')
+
+        # 现在一个叫做佛朗西斯的新用户访问了网站
+        # 确保之前用户A的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Chrome(executable_path='../chromedriver')
+
+        # 佛朗西斯访问首页,并且看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 佛朗西斯输入了一个新待办事项,新建一个清单
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotIn(francis_list_url, edith_list_url)  # 排除和用户A重复
+
+        # 这一页还是没有用户A的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # self.fail('Finish the test')
+
 
 '''
 browser = webdriver.Chrome(executable_path='./chromedriver')
