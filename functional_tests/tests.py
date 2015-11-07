d1d3d2e6bf664c81c0b5a9bc4961f5e7ecbf585b
gubaoer <gubaoer@hotmail.com>
2015-11-07 21:32:45 +0800

    # make functional_tests an app, use LiveserverTestcase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 # coding: utf-8
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time # 为了调试,可用time.sleep
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path='../chromedriver')
         self.browser.implicitly_wait(3)
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 打开首页
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # 确定断网页的标题和头部都包含了'TO-DO'这个词
         self.assertIn('To-Do', self.browser.title)
@@ -50,10 +51,6 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test')
-
-
-if __name__ in '__main__':
-    unittest.main(warnings='ignore')
 
 '''
 browser = webdriver.Chrome(executable_path='./chromedriver')
