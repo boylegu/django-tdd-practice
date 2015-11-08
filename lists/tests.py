ff61c03c9abc8836815ae5f27106779030d38166
gubaoer <gubaoer@hotmail.com>
2015-11-08 20:09:00 +0800

    # new uRl + view for adding to existing lists. Ft oasses :-)

--- lists/tests.py
+++ lists/tests.py
@@ -25,19 +25,30 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_users_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):  # 比test_home_page_displays_all_list_items()方法更通用,并且不用直接调用视图函数
-        list_ = List.objects.create()
+    def test_displays_only_items_for_that_list(self):  # 比test_home_page_displays_all_list_items()方法更通用,并且不用直接调用视图函数
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
-
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')  # 也不需要用response.content.decode()
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id, ))
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
@@ -55,14 +66,42 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': "A new list item"}
         )
+        new_list = List.objects.first()
 
         # self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 
 class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
-        list_ = List()   # list_ 是为了防止和python内置的list方法进行重名,因此才用此变量
+        list_ = List()  # list_ 是为了防止和python内置的list方法进行重名,因此才用此变量
         list_.save()
         first_item = Item()
         first_item.text = 'The first(ever) list item'
--- superlists/urls.py
+++ superlists/urls.py
@@ -11,8 +11,10 @@ urlpatterns = patterns('',
 
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', 'lists.views.home_page', name='home'),
-                       url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+                       url(r'^lists/(\d+)/$', 'lists.views.view_list',
                            name='view_list'
                            ),
-                       url('^lists/new$', 'lists.views.new_list', name='new_list')
+                       url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+
+                       url('^lists/new$', 'lists.views.new_list', name='new_list'),  # 操作数据库不需要加/
                        )
