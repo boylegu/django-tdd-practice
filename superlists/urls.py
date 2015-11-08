88852a8f9317627848a158ef1e1afb0f102e8561
gubaoer <gubaoer@hotmail.com>
2015-11-08 20:13:25 +0800

    # 6.10

--- superlists/urls.py
+++ superlists/urls.py
@@ -9,12 +9,7 @@ urlpatterns = patterns('',
                        # url(r'^$', 'superlists.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
 
-                       url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', 'lists.views.home_page', name='home'),
-                       url(r'^lists/(\d+)/$', 'lists.views.view_list',
-                           name='view_list'
-                           ),
-                       url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-
-                       url('^lists/new$', 'lists.views.new_list', name='new_list'),  # 操作数据库不需要加/
+                       url(r'^lists/', include('lists.urls')),
                        )
