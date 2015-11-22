0f8bdc3716f765feb564277350acf347ba6d5964
gubaoer <gubaoer@hotmail.com>
2015-11-22 18:57:19 +0800

    Rafactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -6,6 +6,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-                       url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
                        url('^new$', 'lists.views.new_list', name='new_list'),  # 操作数据库不需要加/
                        )
